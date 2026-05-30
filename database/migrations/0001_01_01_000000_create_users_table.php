b561cbde870fcc10e959f9724d3b65ee7be7c925
achmadhabibi . <achmad.habibie68@gmail.com>
2026-05-30 15:41:17 +0700

    fix: increase users.nik column length limit to 20 to support NIP-based logins

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('nik', 16)->unique();
+            $table->string('nik', 20)->unique();
             $table->string('email')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
